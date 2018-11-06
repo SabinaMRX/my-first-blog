b2930d1bc42ff02be31e6d94ae077cf49351e5dc
Sabina Marx <sabina.marx@sneo.io>
2018-11-06 20:57:26 +0100

    bugfix

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.login, name='login'),
-    path('accounts/logout/', views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
     path('', include('blog.urls')),
 ]
