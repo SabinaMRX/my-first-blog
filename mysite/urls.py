f0262a4b1dcfae53b829c2c057c6cd435623fa46
Sabina Marx <sabina.marx@sneo.io>
2018-11-06 21:33:29 +0100

    add logged_out.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,6 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/login/', views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', views.LogoutView.as_view(), name='logout'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
 ]
